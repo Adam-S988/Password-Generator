eb0d6adfc2e8b5128f5897d4d212207baf4fde35
Adam-S988 <adam.stevenson@keyin.com>
2024-09-19 14:49:27 -0230

    Added in the ability to add symbols to the password, and added to the README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # QAP1 - Password Generator
+
+Author: Adam Stevenson
+
+Description: This program helps the user create randomly generated passwords. Password by defaul are 8 characters, and are lower case, but, passwords can be customized with uppercase letters, numbers, and symbols. The length of the password can also be modified to be between 5 and 10 characters if desired.
--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 #!/usr/bin/env node
 
+// Adam Stevenson
+
 const process = require("node:process");
 
 let passwordLength = 8;
 let allowNumbers = false;
 let allowUppercase = false;
+let allowSymbols = false;
 
+//Opening text
 function printOpenMessage() {
   console.log(`
     Welcome to the Password Generator.
 
     For help, please type '--help' or '-h'.
-    `);
+  `);
 }
 
+//Help screen
 function printHelpMessage() {
   console.log(`
     Help for Password Generator:
@@ -22,62 +27,83 @@ function printHelpMessage() {
       - Type '--length' or '-l' followed by an integer to change the password length (5-10).
       - Type '--num' or '-n' to allow integers in the password.
       - Type '--maj' or '-m' to allow uppercase letters in the password.
+      - Type '--symbol' or '-s' to allow for symbols in the password.
       !!Do not share your password with anyone!!
-    `);
+  `);
 }
 
+// Error if the password character length falls outside of the valid range
 function printLengthMessage() {
   console.log(`
     ERROR: Password length must be between 5 and 10 characters.
-    `);
+  `);
 }
 
+// Error if the argument for the actual password creation is missing
 function printMissingCreateArgumentMessage() {
   console.log(`
     ERROR: You must include the '--create' or '-c' argument to specify the password.
-    `);
+  `);
 }
 
+// Message if password length is modified
 function printPasswordLengthSetMessage(length) {
-  console.log(`
-    Password length set to ${length} characters.
-    `);
+  console.log(`Password length set to ${length} characters.`);
 }
 
-function printPasswordErrorMessage(length) {
-  console.log(`
-    ERROR: Password must be exactly ${length} characters long.
-    `);
-}
-
+//Defining the charsets
 function generatePassword(length) {
   const lowercaseCharset = "abcdefghijklmnopqrstuvwxyz";
   const uppercaseCharset = allowUppercase ? "ABCDEFGHIJKLMNOPQRSTUVWXYZ" : "";
   const numberCharset = allowNumbers ? "0123456789" : "";
-
-  const charset = lowercaseCharset + uppercaseCharset + numberCharset;
+  const symbolCharset = allowSymbols ? "!@#$%^&*()+_=-{}[]|;:,.<>?/~`" : "";
 
   let password = "";
-  for (let i = 0; i < length; i++) {
+
+  // Ensure at least one character from each required set
+  if (allowUppercase)
+    password +=
+      uppercaseCharset[Math.floor(Math.random() * uppercaseCharset.length)];
+  if (allowNumbers)
+    password += numberCharset[Math.floor(Math.random() * numberCharset.length)];
+  if (allowSymbols)
+    password += symbolCharset[Math.floor(Math.random() * symbolCharset.length)];
+
+  // Fill the rest of the password length with random characters from the full charset
+  const charset =
+    lowercaseCharset + uppercaseCharset + numberCharset + symbolCharset;
+  while (password.length < length) {
     const randomIndex = Math.floor(Math.random() * charset.length);
     password += charset[randomIndex];
   }
+
+  // Shuffle the password to randomize character positions
+  password = password
+    .split("")
+    .sort(() => Math.random() - 0.5)
+    .join("");
+
   return password;
 }
 
+//Valid charsets
 function validatePassword(password) {
   const hasLetter = /[a-zA-Z]/.test(password);
   const hasNumber = /[0-9]/.test(password);
   const hasUppercase = /[A-Z]/.test(password);
+  const hasSymbols = /[!@#$%^&*()+_=\-{}\[\]|;:,.<>?/~`]/.test(password);
+
   return (
     hasLetter &&
-    (allowNumbers ? true : !hasNumber) &&
-    (allowUppercase ? hasUppercase : true)
+    (allowNumbers ? hasNumber : true) &&
+    (allowUppercase ? hasUppercase : true) &&
+    (allowSymbols ? hasSymbols : true)
   );
 }
 
 const userArguments = process.argv.slice(2);
 
+//Change password character length
 const lengthArgIndex =
   userArguments.indexOf("--length") !== -1
     ? userArguments.indexOf("--length")
@@ -94,64 +120,75 @@ if (lengthArgIndex !== -1 && lengthArgIndex + 1 < userArguments.length) {
   }
 }
 
+//Show Help screen
 if (userArguments[0] === "--help" || userArguments[0] === "-h") {
   printHelpMessage();
   return;
 }
 
+//Create a password
 const createArgIndex =
   userArguments.indexOf("--create") !== -1
     ? userArguments.indexOf("--create")
     : userArguments.indexOf("-c");
 
+if (createArgIndex === -1) {
+  printMissingCreateArgumentMessage();
+  return;
+}
+
+// Numeral arguments
 const numArgIndex =
   userArguments.indexOf("--num") !== -1
     ? userArguments.indexOf("--num")
     : userArguments.indexOf("-n");
 
+// Majuscule arguments
 const majArgIndex =
   userArguments.indexOf("--maj") !== -1
     ? userArguments.indexOf("--maj")
     : userArguments.indexOf("-m");
 
+// Symbol arguments
+const symArgIndex =
+  userArguments.indexOf("--symbol") !== -1
+    ? userArguments.indexOf("--symbol")
+    : userArguments.indexOf("-s");
+
 if (numArgIndex !== -1) {
-  if (createArgIndex === -1) {
-    printMissingCreateArgumentMessage();
-    return;
-  }
   allowNumbers = true; // Set the flag to allow numbers
   console.log("Numbers are allowed in the password.");
 }
 
 if (majArgIndex !== -1) {
-  if (createArgIndex === -1) {
-    printMissingCreateArgumentMessage();
-    return;
-  }
   allowUppercase = true; // Set the flag to allow uppercase letters
   console.log("Uppercase letters are allowed in the password.");
 }
 
-if (createArgIndex !== -1) {
-  const password = generatePassword(passwordLength);
+if (symArgIndex !== -1) {
+  allowSymbols = true; // Set the flag to allow symbols
+  console.log("Symbols are allowed in the password.");
+}
 
-  if (password.length !== passwordLength) {
-    printPasswordErrorMessage(passwordLength);
-    return;
-  }
+const password = generatePassword(passwordLength);
 
-  if (!validatePassword(password)) {
-    console.log(
-      `ERROR: Generated password does not meet complexity requirements.`
-    );
-    return;
-  }
-
-  console.log(`
-Generated password: ${password}
-  `);
+if (password.length !== passwordLength) {
+  printPasswordErrorMessage(passwordLength);
   return;
 }
+
+// Error if something occurs with generating a password
+if (!validatePassword(password)) {
+  console.log(
+    `ERROR: Generated password does not meet complexity requirements.`
+  );
+  return;
+}
+
+// Shows the randomly generated password
+console.log(`
+Generated password: ${password}
+`);
 
 if (userArguments.length === 0) {
   printOpenMessage();
@@ -160,4 +197,4 @@ if (userArguments.length === 0) {
 
 console.log(`
 Password accepted.
-  `);
+`);
